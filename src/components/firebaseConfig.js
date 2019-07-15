b1592d362b0d58db8cd2ad5070f80f5772140e5d
Jennifer <floxzyjay@gmail.com>
2019-07-15 21:06:18 +0100

    First COMMIT

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js"></script>
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyAOVgRcqy670o9eGsXtvnCm_Ee_90AVR9w',
+  authDomain: 'elvon-cma.firebaseapp.com',
+  databaseURL: 'https://elvon-cma.firebaseio.com',
+  projectId: 'elvon-cma',
+  storageBucket: '',
+  messagingSenderId: '498022760618',
+  appId: '1:498022760618:web:45474d6b66ab6709'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
